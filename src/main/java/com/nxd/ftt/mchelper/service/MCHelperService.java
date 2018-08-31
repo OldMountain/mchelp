aa9338a9f91850f39a0750c6303a12f686cb190f
OldMountain <1351395043@qq.com>
2018-08-31 10:09:15 +0800

    提交

---
+++ src/main/java/com/nxd/ftt/mchelper/service/MCHelperService.java
@@ -0,0 +1,15 @@
+package com.nxd.ftt.mchelper.service;
+
+import com.nxd.ftt.mchelper.entity.AuthMe;
+
+import java.util.List;
+
+/**
+ * MCHelperService
+ *
+ * @author luhangqi
+ * @date 2018/6/5
+ */
+public interface MCHelperService {
+    List<AuthMe> getAuthMe();
+}
